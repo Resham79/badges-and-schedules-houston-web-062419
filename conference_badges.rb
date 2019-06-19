c47c2e8a09a191f5ae9ca362a62c1d630de9c6b0
Resham Arora <resham.arora@gmail.com>
2019-06-19 01:16:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  newarray = []
+  attendees.each do |name|
+    newarray.push("Hello, my name is #{name}.")
+  end
+  return newarray
+end
+
+
